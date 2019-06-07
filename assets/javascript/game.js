5f81cc42b142b0433834dbf30135a845fa05dd9e
Balince <mbal139@gmail.com>
2019-06-06 21:15:51 -0400

    few things missing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var letters;
 
 var compChoices = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
@@ -10,30 +11,35 @@ var userChoiceText = document.getElementById("userChoice-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var gLText = document.getElementById("gL-text");
+var lettersText = document.getElementById("letters-text");
 
 document.onkeydown = function(event) {
-
+    
+var letterA = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+ 
 var userGuess = event.key;
 
 var compGuess = compChoices [Math.floor(Math.random() * compChoices.length)];
 
-if (userGuess === 'abcdefghijklmnopqrstuvwxyz'.split('')) {
 
 if (compGuess === userGuess) {
-wins++
+wins++, guessesLeft = 9
 }
-if (compGuess !== userGuess) {
+
+else if (compGuess !== userGuess) {
 guessesLeft--
 }
-if (guessesLeft = 0) { 
-losses++
+
+if (guessesLeft < 1) { 
+losses++, guessesLeft = 9
 }
+
 
 
 userChoiceText.textContent = "You chose: " + userGuess;
 winsText.textContent = "Wins: " + wins;
 lossesText.textContent = "Losses: " + losses;
 gLText.textContent = "Guesses left: " + guessesLeft;
-}
+lettersText.textContent = "Letters you've tried: " + letters;
 
 };
